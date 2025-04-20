11634252c908155675638bc1dd36e91f7e8f5793
vlad <vlad>
2025-04-20 19:48:05 +0300

    waybar update

--- waybar/Scripts/player.sh
+++ waybar/Scripts/player.sh
@@ -2,19 +2,25 @@
 
 player="firefox"
 
-status="$(playerctl --player=$player status 2>/dev/null)"
-if [ "$status" = "" ]; then
-  echo " ⏹ "
-  exit
-elif [ "$status" = "Playing" ]; then
-  text="$(playerctl --player=$player metadata --format '⏸ {{ artist }} - {{ title }}')"
-elif [ "$status" = "Paused" ]; then
-  text="$(playerctl --player=$player metadata --format '▶ {{ artist }} - {{ title }}')"
-fi
+while true; do
+  status="$(playerctl --player=$player status 2>/dev/null)"
+  
+  if [ "$status" = "" ]; then
+    output=" ⏹ "
+  elif [ "$status" = "Playing" ]; then
+    text="$(playerctl --player=$player metadata --format '⏸ {{ artist }} - {{ title }}')"
+  elif [ "$status" = "Paused" ]; then
+    text="$(playerctl --player=$player metadata --format '▶ {{ artist }} - {{ title }}')"
+  fi
 
-if [ ${#text} -gt 60 ]; then
-  echo " ${text:0:60}... "
-else
-  echo " ${text} "
-fi
+  # Обрезаем текст, если он слишком длинный
+  if [ ${#text} -gt 40 ]; then
+    output=" ${text:0:40}... "
+  else
+    output=" ${text} "
+  fi
 
+  # Выводим результат (можно добавить `clear` для "обновления" строки)
+  echo "$output"  # `-ne` для вывода в той же строке
+  sleep 0.5  # Задержка 1 секунда
+done
